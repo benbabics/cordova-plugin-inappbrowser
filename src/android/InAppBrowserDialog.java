30f6a81386668930d6ef0e9af447d799cd9ca535
Ben Babics <bbabics@gmail.com>
2016-06-06 15:47:49 -0400

    disable android hardware back button

--- src/android/InAppBrowserDialog.java
+++ src/android/InAppBrowserDialog.java
@@ -46,6 +46,7 @@ public class InAppBrowserDialog extends Dialog {
         if (this.inAppBrowser == null) {
             this.dismiss();
         } else {
+            /*
             // better to go through the in inAppBrowser
             // because it does a clean up
             if (this.inAppBrowser.hardwareBack() && this.inAppBrowser.canGoBack()) {
@@ -53,6 +54,7 @@ public class InAppBrowserDialog extends Dialog {
             }  else {
                 this.inAppBrowser.closeDialog();
             }
+            */
         }
     }
 }
